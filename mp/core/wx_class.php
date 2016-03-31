1d299c3317517cb9e5fe05db802825ad5f1e6fdf
ZhangPeng <sjiaomail@gmail.com>
2016-03-31 19:54:58 +0800

    bug fixed

--- mp/core/wx_class.php
+++ mp/core/wx_class.php
@@ -1,21 +1,24 @@
+
 <?php
 /**
   * wechat php
 */
 class weChat
 {
-	public function valid()
+    public function valid()
     {
         $echoStr = $_GET["echostr"];
         //valid signature , option
         if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
+            echo $echoStr;
+            exit;
         }
     }
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
+        //get post data, May be due to the different environments
+
+        include('ctr_user.php');
         function register($input, $openID){
             if(!$input){
                 return -1;
@@ -26,7 +29,6 @@ class weChat
                 }else{
                     $name = $regData[0];
                     $id = $regData[1];
-                    include('ctr_user.php');
                     $content = array(
                         'userID' => $id,
                         'ecardID' => $id,
@@ -35,15 +37,14 @@ class weChat
                         'regTime' => time(),
                         'status' => 1);
                     addNewUser($openID, $content);
-                    $fp = fopen('id.txt',"a");
-                    fputs($fp,$openID.'|'.$name.'|'.$id);
-                    fclose($fp);                
+                    $fb = fopen('../database/debug.txt',"a");
+                    fputs($fb,date("m/d H:i:s")." Wechat $id|$name|$openID|$input <br>");
+                    fclose($fb);
                     return 1;
                 }
             }
         }
         function transText($keyword, $fromUsername){
-           include('ctr_user.php');
             // 不知道是否能调用同级函数中的include
             if($keyword=="功能" || $keyword=="菜单" || $keyword=="m" ){
                 $transContent = array(
@@ -52,6 +53,7 @@ class weChat
                 );
                 // 以数组的形式返回数据的内容和类型，注意转义符需要用双引号
             }else if(strlen($keyword) < 2){
+
                 $uid = getUserID($fromUsername);
                 switch($keyword){
                     case 1:
@@ -63,13 +65,13 @@ class weChat
                     case 2:
                         $transContent = array(
                             'type' => 'text',
-                            'str' => "戳开链接：http://tikumm.duapp.com/mp/select.php?uid=$uid"
+                            'str' => "戳开链接：http://tikumm.duapp.com/select.php?uid=$uid"
                         );
                     break;
                     case 3:
                         $transContent = array(
                             'type' => 'text',
-                            'str' => "戳开链接：http://tikumm.duapp.com/mp/queue.php?uid=$uid"
+                            'str' => "戳开链接：http://tikumm.duapp.com/queue.php?uid=$uid"
                         );
                     break;                    
                     case 4:
@@ -99,44 +101,44 @@ class weChat
         }
 
 
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-      	//extract post data
-		if (!empty($postStr)){
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //extract post data
+        if (!empty($postStr)){
                 /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
                    the best way is to check the validity of xml by yourself */
                 libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
                 $keyword = trim($postObj->Content);
                 $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            <FuncFlag>0</FuncFlag>
+                            </xml>";             
                 
-				if(!empty( $keyword ))
+                if(!empty( $keyword ))
                 {
                     $strData = transText($keyword, $fromUsername);
-              		$msgType = $strData['type'];
-                	$contentStr = $strData['str'];
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
+                    $msgType = $strData['type'];
+                    $contentStr = $strData['str'];
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
                 }else{
-                	echo "Input something...";
+                    echo "Input something...";
                 }
 
         }else {
-        	echo "";
-        	exit;
+            echo "";
+            exit;
         }
     }
-		
-	private function checkSignature()
-	{
+        
+    private function checkSignature()
+    {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
@@ -145,20 +147,20 @@ class weChat
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        		
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
-		
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 ?>
